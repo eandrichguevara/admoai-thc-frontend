c7a44fec16db9356ca7f2b5ad8728cce6f06f081
Emilio Andrich <eandrichguevara@gmail.com>
2025-10-02 14:08:37 -0300

    feat(QueryProvider): move QueryProvider component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import QueryProvider from './providers/QueryProvider';
+import QueryProvider from '@/providers/QueryProvider';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
